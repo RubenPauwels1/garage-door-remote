c5fd3a9e4f598a4a2e03a186957fada06dd4df66
Ruben Pauwels <rubenp.pauwels@gmail.com>
2018-06-19 18:41:45 +0200

    Added external-ip to write the IP to the Firebase DB

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const express = require('express')
 const colors = require('colors')
 const firebase = require('firebase');
 const env = require('dotenv').config()
+const getIP = require('external-ip')();
+
 var bodyParser = require('body-parser')
 
+//----------------
+//Firebase config
+//----------------
 
 var config = {
     apiKey: process.env.APIKEY,
@@ -20,19 +25,44 @@ var jsonParser = bodyParser.json()
 const app = express()
 const port = 9000;
 
+//------------------------------
+//Write external ip to Firebase
+//------------------------------
+
+function writeIP() {
+    var database = firebase.database();
+
+    getIP((err, ip) => {
+        if (err) {
+            // every service in the list has failed
+            throw err;
+        }
+        
+        firebase.database().ref('/').set({
+            ip: ip,
+        }).then(function() {
+            console.log(`${ip} written to DB`);
+        }).catch(function() {
+            console.log(`connot write IP to DB`);
+        });
+    });
+}
+
+setInterval(writeIP, 60000);
+
+//---------------
+//Listen to port
+//---------------
+
 app.listen(port, () => {
     console.log('GARAGE DOOR OPENER'.rainbow.bold);
     console.log(colors.white.italic('Running on port'), colors.red.bold(`${port}`))
 })
 
-
-app.get('/', (req, res) => {
-    console.log('Home')
-
-    res.send('Welcome home')
-})
-
+//---------------------------------------------------------------------------
 //POST: localhost:9000/login with {"email": "YOUREMAIL", "pass": "YOURPASS"}
+//---------------------------------------------------------------------------
+
 app.post('/login', jsonParser, (req, res) => {
     console.log('Trying to log in with email', req.body.email);
 
