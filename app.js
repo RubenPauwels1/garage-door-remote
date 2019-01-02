f065cfe2af227586f97b97f64f1fb8c381d07d18
Ruben Pauwels <rubenp.pauwels@gmail.com>
2019-01-02 16:48:38 +0100

    Added auto updating for IP

--- app.js
+++ app.js
@@ -2,15 +2,37 @@ const express = require('express')
 
 const env = require('dotenv').config()
 const firebase = require('firebase')
+const publicIp = require('public-ip');
 
 const firebaseConfig = require('./firebaseConfig')
 const routes = require('./routes/routes')
 
 const firebaseApp = firebase.initializeApp(firebaseConfig.config)
+const database = firebase.database();
 
 const app = express()
 const port = process.env.PORT
 
 app.use('/api', routes);
+
+const interval = 60000;
+
+async function getIP() {
+	try {
+		const ip = await publicIp.v4();
+
+		database.ref('ip').set({
+			ip
+		}).then(function(){
+			console.log('IP', `${ip} written to DB`);
+		});
+	} catch (err) {
+		console.log(err);
+	}
+
+	setTimeout(getIP, interval);
+}
+
+getIP();
 
 app.listen(port, () => console.log(`Listening on port ${port}!`))
