6944db012db2e25c45a1ec8443ce20ead003a65f
Ruben Pauwels <rubenp.pauwels@gmail.com>
2019-04-14 18:07:51 +0200

    Added status to Firebase DB

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const env = require('dotenv').config()
 const firebase = require('firebase')
 const publicIp = require('public-ip');
+const { exec } = require('child_process');
 
 const firebaseConfig = require('./firebaseConfig')
 const routes = require('./routes/routes')
@@ -15,7 +16,8 @@ const port = process.env.PORT
 
 app.use('/api', routes);
 
-const interval = 60000;
+const interval  = 60000;
+const interval2 = 2000;
 
 async function getIP() {
 	try {
@@ -33,6 +35,20 @@ async function getIP() {
 	setTimeout(getIP, interval);
 }
 
+function checkIfOpen() {
+	// GPIO PIN 15
+	exec('gpio -g read 15', (error, stdout, stderr) => {
+		const isOpen = parseInt(stdout) === 1 ? 'closed' : 'open';
+
+		database.ref('status').set({
+			isOpen
+		})
+	});
+
+	setTimeout(checkIfOpen, interval2);
+}
+
 getIP();
+checkIfOpen();
 
 app.listen(port, () => console.log(`Listening on port ${port}!`))
